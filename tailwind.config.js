0a72d66d81a42d05feb26351c7378f626fdc6ae1
Mohammad Reza <en.salemi@outlook.com>
2022-09-23 07:45:32 +0330

    base: setup 'daisyUI' and install 'react-icons'

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,19 @@ module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
+  darkMode: 'class',
   theme: {
     extend: {},
   },
   plugins: [require("daisyui")],
+  daisyui: {
+    styled: true,
+    themes: false,
+    base: true,
+    utils: true,
+    logs: true,
+    rtl: false,
+    prefix: "",
+    darkTheme: "dark",
+  },
 }
