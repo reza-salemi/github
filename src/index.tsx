8ba044ad54b300510e62e74d6f5d57561a56d2df
Mohammad Reza <en.salemi@outlook.com>
2022-09-26 20:18:22 +0330

    feat: Implement user context for authentication

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {BrowserRouter} from 'react-router-dom';
+import {UserProvider} from "./contexts/user.context";
 import './index.css';
 import App from './App';
 
@@ -8,7 +10,11 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <UserProvider>
+        <App/>
+      </UserProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
