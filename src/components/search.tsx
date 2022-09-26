02eceb808c2dea5151224afc43b48ebd7b638a6f
Mohammad Reza <en.salemi@outlook.com>
2022-09-26 20:10:28 +0330

    feat: create 'search' component

---
+++ src/components/search.tsx
@@ -0,0 +1,9 @@
+const SearchBar = () => {
+  return (
+    <div className="form-control">
+      <input type="text" placeholder="Search" className="input input-sm input-bordered"/>
+    </div>
+  );
+};
+
+export default SearchBar;
