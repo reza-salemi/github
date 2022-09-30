600c39827022d32d244c7cedfcc46f7f150ce2b6
Mohammad Reza <en.salemi@outlook.com>
2022-09-30 17:46:58 +0330

    update: change background color and add marin, get current user route

--- src/components/layouts/header.tsx
+++ src/components/layouts/header.tsx
@@ -1,21 +1,17 @@
 import {FaGithub} from "react-icons/fa";
 import {useContext} from "react";
 import {Link} from "react-router-dom";
-import {signInWithGitHubPopup, userSignOut} from "../../utils/firebase/config";
-
-import {UserContext} from "../../contexts/user.context";
+import {userSignOut} from "../../utils/firebase/config";
+import {AuthContext} from "../../contexts/auth.context";
 import SearchBar from "../search";
 import ToggleTheme from "../toggle-theme";
+import SignIn from "../sign-in";
 
 const Header = () => {
-  const currentUser = useContext(UserContext);
-
-  const logGitHubUser = async () => {
-    await signInWithGitHubPopup();
-  };
+  const {firebaseUser,currentUser} = useContext(AuthContext);
 
   return (
-    <header className="dark:bg-neutral bg-blue-600 text-neutral-content">
+    <header className="dark:bg-neutral mb-8 bg-blue-500 text-neutral-content">
       <nav className="navbar container mx-auto">
         <div className="flex-1 gap-3">
           <Link to="/">
@@ -25,28 +21,28 @@ const Header = () => {
         </div>
 
         <div className="flex-none gap-1">
-          {!currentUser ?
+          {!firebaseUser ?
             (<div className="form-control">
-              <button className="btn btn-sm btn-ghost" onClick={logGitHubUser}>SIGN IN</button>
+                <SignIn />
             </div>) :
             (<div className="dropdown dropdown-end">
               <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
                 <div className="w-10 rounded-full">
-                  <img src={currentUser.photoURL || ""} alt="Profile Avatar"/>
+                  <img src={firebaseUser.photoURL || ""} alt="UserProfile Avatar"/>
                 </div>
               </label>
               <ul tabIndex={0}
-                  className="mt-3 p-2 shadow menu menu-compact dropdown-content dark:bg-neutral bg-blue-600 rounded-box w-52 text-neutral-content">
+                  className="mt-3 p-2 shadow menu menu-compact dropdown-content dark:bg-neutral bg-blue-500 rounded-box w-52 text-neutral-content">
                 <li>
-                  <span className="justify-between">
-                    Profile
-                  </span>
+                  <Link to={`/user/${currentUser?.login}`} className="justify-between">
+                    User profile
+                  </Link>
                 </li>
                 <li onClick={userSignOut}><span>Logout</span></li>
               </ul>
             </div>)
           }
-          <ToggleTheme />
+          <ToggleTheme/>
         </div>
       </nav>
     </header>
