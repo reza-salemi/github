41294dd7aa400c45f1312e5d70041249d1e2c7a4
Mohammad Reza <en.salemi@outlook.com>
2022-10-02 09:57:51 +0330

    fix: get current user info from firebase

--- src/contexts/auth.context.tsx
+++ src/contexts/auth.context.tsx
@@ -1,33 +1,46 @@
 import {createContext, useState, useEffect, ReactNode} from "react";
-import {onAuthStateChangedListener} from "../utils/firebase/config";
-import {User} from 'firebase/auth';
-import {getCurrentUser} from "../api/user";
-import {GithubUser} from "../types/user.type";
+import {onAuthStateChangedListener,signInWithGitHubPopup} from "../utils/firebase/config";
+import {User,getAdditionalUserInfo,GithubAuthProvider} from 'firebase/auth';
 
 interface AuthContextInterface {
   firebaseUser: User | null,
-  currentUser: GithubUser | null
+  logGitHubUser: () => void;
+
 }
 
 export const AuthContext = createContext<AuthContextInterface>({
   firebaseUser: null,
-  currentUser: null
+  logGitHubUser: () => null
 });
 
 export const AuthProvider = ({children}: { children: ReactNode }) => {
   const [firebaseUser, setFirebaseUser] = useState<User | null>(null);
-  const [currentUser,setCurrentUser] = useState(null);
+
+  const logGitHubUser = () => {
+    signInWithGitHubPopup().then((result) => {
+      const credential = GithubAuthProvider.credentialFromResult(result);
+      const token = credential?.accessToken;
+      const details = getAdditionalUserInfo(result);
+      if (typeof token === "string") {
+        localStorage.setItem('github-token', token);
+        // @ts-ignore
+        localStorage.setItem('username', details?.username)
+      }
+    }).catch((error) => {
+      console.log(error.message)
+    });
+  };
 
   useEffect(() => {
     const unSubscribe = onAuthStateChangedListener((user) => {
-      setFirebaseUser(user)
-      getCurrentUser().then(json => setCurrentUser(json));
+      setFirebaseUser(user);
       if (!user) {
         localStorage.removeItem('github-token');
+        localStorage.removeItem('username');
       }
     });
     return unSubscribe;
   }, []);
 
-  return <AuthContext.Provider value={{firebaseUser,currentUser}}>{children}</AuthContext.Provider>;
+  return <AuthContext.Provider value={{firebaseUser,logGitHubUser}}>{children}</AuthContext.Provider>;
 };
