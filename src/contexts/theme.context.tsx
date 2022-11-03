d9c4f7a56c24104f9ce03308b35e1ed65d2170e6
Mohammad Reza <en.salemi@outlook.com>
2022-11-03 10:30:22 +0330

    update: remove getInitialTheme function

--- src/contexts/theme.context.tsx
+++ src/contexts/theme.context.tsx
@@ -5,40 +5,23 @@ interface ThemeContextInterface {
   setTheme: (theme: string) => void
 }
 
-const getInitialTheme = () => {
-  if (typeof window !== 'undefined' && window.localStorage) {
-    const currentTheme = window.localStorage.getItem('current-theme');
-    if (typeof currentTheme === 'string') {
-      return currentTheme;
-    }
-    if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
-      return 'dark';
-    }
-  }
-  return 'light';
-};
-
 export const ThemeContext = createContext<ThemeContextInterface>({
   theme: '',
   setTheme: () => null,
 });
 
 export const ThemeProvider = ({children}: { children: ReactNode }) => {
-  const [theme, setTheme] = useState(getInitialTheme);
-
-  const checkTheme = (existing: string) => {
-    const htmlElement = window.document.documentElement;
-    const isDark = existing === 'dark';
-
-    htmlElement.classList.remove(isDark ? 'light' : 'dark');
-    htmlElement.classList.add(existing);
-
-    localStorage.setItem('current-theme', existing);
-  };
+  const [theme, setTheme] = useState(() => localStorage.getItem('theme') ?? 'light');
+  const colorTheme = theme === 'dark' ? 'light' : 'dark';
 
   useEffect(() => {
-    checkTheme(theme);
-  }, [theme]);
+    const htmlElement = window.document.documentElement;
+    console.log(theme);
+    htmlElement.classList.remove(colorTheme);
+    htmlElement.classList.add(theme);
+
+    localStorage.setItem('theme', theme);
+  }, [colorTheme, theme]);
 
   return (
     <ThemeContext.Provider value={{theme, setTheme}}>
